a607ec39be0f772f97d55a3df670adcb50810a6c
Sergey <sergey.skrebnev@gmail.com>
2024-12-25 14:04:20 +0300

    ReadTheDocs step 6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = "2024, Sergey Skrebnev"
 author = "Sergey Skrebnev"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.1.2"
 
 # -- General configuration ---------------------------------------------------
 
